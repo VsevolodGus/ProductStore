f6bdcf349838253317c5444b1364d8de2a4f9ffb
Сева Гусак <sevagusak@outlook.com>
2021-05-27 15:51:34 +0500

    Добавил класс заказа и  элемент заказа с его описанием

---
+++ domain/Store/Order.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Store
+{
+    public class Order
+    {
+        private readonly List<OrderItem> items;
+
+        public int Id { get; set; }
+        public int TotalCount => items.Sum(item => item.Count);
+
+        public decimal TotalPrice => items.Sum(item => item.Count * item.Price);
+
+        public Order(int id, IEnumerable<OrderItem> items)
+        {
+            if (items == null)
+                throw new ArgumentNullException("no correct source OrderItem for Order");
+
+            Id = id;
+
+            this.items = items.ToList();
+        }
+
+        public IReadOnlyCollection<OrderItem> Items
+        {
+            get { return items; }
+        }
+
+        public OrderItem GetItemById(int id)
+        {
+            int index = items.FindIndex(item => item.ProductId == id);
+
+            if (index == -1)
+                throw new ArgumentException($"Not found OrderItrem with {id} in the Order");
+            
+            return items[index];
+        }
+
+        public void RemoveOrderItem(int id)
+        {
+            int index = items.FindIndex(item => item.ProductId == id);
+
+            if (index == -1)
+                throw new ArgumentException($"Not found OrderItrem with {id} in the Order");
+
+            if (items[index].Count > 1)
+                items[index].Count -= 1;
+            else 
+                items.RemoveAt(index);
+        }
+
+        public void RemoveFullOrderItem(int id)
+        {
+            int index = items.FindIndex(item => item.ProductId == id);
+
+            if (index == -1)
+                throw new ArgumentException($"Not found OrderItrem with {id} in the Order");
+
+            items.RemoveAt(index);
+        }
+
+        public void AddOrUpdate(Product product, int count)
+        {
+            if (product == null)
+                throw new ArgumentNullException("when adding order, passing null product");
+            else if(count <= 0)
+                throw new ArgumentNullException("when adding order, passing no correct count");
+
+
+            int index = items.FindIndex(item => item.ProductId == product.Id);
+
+
+            if (index == -1)
+                items.Add(new OrderItem(product.Id, count, product.Price));
+            else
+                items[index].Count += count;
+        }
+    }
+}
---
+++ domain/Store/OrderItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Store
+{
+    public class OrderItem
+    {
+        public int ProductId;
+
+        private int count;
+
+        public int Count
+        {
+            get => count;
+            set 
+            {
+                ThrowExceptionForNoCorrectCount(value);
+
+                count = value;
+            }
+        }
+
+        public decimal Price;
+
+        public OrderItem(int id, int count, decimal price)
+        {
+            ProductId = id;
+            Count = count;
+            Price = price;
+        }
+
+        private static void ThrowExceptionForNoCorrectCount(int count)
+        {
+            if (count <= 0)
+                throw new ArgumentException("no correct value count");
+        }
+    }
+}
