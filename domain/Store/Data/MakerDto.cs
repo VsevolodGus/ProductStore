3424e7ad3b2fd72152e21d168d4fc4708e601ca7
VsevolodGus <gusakseva8@gmail.com>
2021-06-24 23:20:27 +0500

    Добавил шаблоны для БД, префиск DTO(Data Transfer Object)

---
+++ domain/Store/Data/MakerDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Store.Data
+{
+    public class MakerDto
+    {
+        public int Id { get; }
+
+        public string Title { get; }
+
+        public string NumberPhone { get; }
+
+        public string Email { get; }
+
+        public string Addres { get; }
+
+        public string Description { get; }
+    }
+}
---
+++ domain/Store/Data/PrdocutDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Store.Data
+{
+    public class PrdocutDto
+    {
+        public int Id { get; }
+
+        public string Title { get; }
+
+        public int IdMaker { get; }
+
+        public string Category { get; }
+
+        public decimal Price { get; }
+
+        public string Description { get; }
+    }
+}
